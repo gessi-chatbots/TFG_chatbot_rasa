25a013bb6bfc9c76e98be6a7048c3f7f81dce318
Bran <bran.rodriguez@est.fib.upc.edu>
2021-11-27 23:25:35 +0100

    Docker working

--- start_services.sh
+++ start_services.sh
@@ -1,3 +1,4 @@
-cd app/
-# Start rasa server with nlu model
-rasa run --enable-api --cors "*" --debug && rasa run actions
+cd ./app
+rasa train
+rasa run --enable-api --cors "*" &
+rasa run actions &
