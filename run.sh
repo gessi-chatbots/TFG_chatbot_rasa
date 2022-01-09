020fd3e7a9cf0c02f76073b0bfe38a5ca6d0160e
Bran <bran.rodriguez@est.fib.upc.edu>
2022-01-09 21:21:12 +0100

    Better entity identification #minor

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/ec2-user/rasa/git/TFG_chatbot_rasa
+docker-compose down
 docker-compose build
 docker-compose up -d
-docker exec -it tfg_chatbot_rasa-postgres-1 psql -U project_admin -d rasa -f scripts/queries.sql
