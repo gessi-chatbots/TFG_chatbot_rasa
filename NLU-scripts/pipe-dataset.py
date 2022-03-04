858d96e98f70be6884bf3f3728e9b87af1d47d0e
carlacampas <ccg.campas@gmail.com>
2022-03-04 16:22:25 +0100

    add cmds for rasa

--- NLU-scripts/pipe-dataset.py
+++ NLU-scripts/pipe-dataset.py
@@ -1,5 +1,6 @@
 from termcolor import colored
 import sys
+import os
 
 args = sys.argv[1:]
 
@@ -7,7 +8,7 @@ if '-h' in args or '--help' in args:
     print(colored('positional arguments', attrs=["bold"]))
     print('replace <path>              Replace data foun ')
     print('add <path>                  Add data to existing set ')
-    print('path for add/replace should lead to the data folder in your rasa chatbot')
+    print('path to rasa instance')
     print()
     print(colored('optional arguments', attrs=["bold"]))
     print('-n <path>            replace/add NLU data found in specified set ')
@@ -26,9 +27,9 @@ for i in range(2, len(args)):
     x = args[i]
     y = args[i+1]
     file = ""
-    if x.strip() == '-n': file = '/nlu.yml'
-    elif x.strip() == '-r': file = '/rules.yml'
-    elif x.strip() == '-s': file = '/stories.yml'
+    if x.strip() == '-n': file = '/data/nlu.yml'
+    elif x.strip() == '-r': file = '/data/rules.yml'
+    elif x.strip() == '-s': file = '/data/stories.yml'
     else: raise Exception("ERROR: wrong optional argument specified")
     
     lines = open(y).readlines()
@@ -37,3 +38,8 @@ for i in range(2, len(args)):
     with open(path + file, type) as f:
         for l in lines: f.write(l)
         f.close()
+
+os.chdir(path)
+os.system('rasa train')
+os.system('rasa shell')
+
